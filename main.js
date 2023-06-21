4b9ff99b1f0f4af70d3fb1ef92d84e855d888c8d
jeffureta <jeffvincentureta@gmail.com>
2023-06-21 12:05:59 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+$('body')
+    .css(
+        { 
+            
+            'background-color': '#27374D',
+            'color':'#DDE6ED',
+        }
+        )
+$('div:first').addClass('container')
+        .css(
+            {
+                "margin-top": "5%",
+            }
+        )
+
+$('header').addClass('text-center')
+
+$('div div').addClass(['d-flex', 'justify-content-center', 'align-items-center'])
+
+$('img')
+    .css(
+        {
+            'max-width': '80%',
+            'min-width': '50%',
+            'height': 'auto',
+            'background-color': '#27374D',
+        }
+    )
+
+$("article p").css({
+    "text-align": "justify",
+    "text-justify": "auto",
+})
